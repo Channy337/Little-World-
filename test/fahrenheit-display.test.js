660f3e0c4c5c1e0c9df70430f8ce103d2354c11d
Channy337 <cdoan337@gmail.com>
2026-09-12 21:50:27 -0500

    Update Fahrenheit UI test for V0.11

--- test/fahrenheit-display.test.js
+++ test/fahrenheit-display.test.js
@@ -3,10 +3,10 @@ const test=require('node:test');
 const assert=require('node:assert/strict');
 const fs=require('node:fs');
 
-test('V0.10.1 displays visitor weather in Fahrenheit without changing canonical Celsius',()=>{
+test('V0.11.0 displays visitor weather in Fahrenheit without changing canonical Celsius',()=>{
   const html=fs.readFileSync('index.html','utf8');
   const js=fs.readFileSync('fahrenheit-display.js','utf8');
-  assert.match(html,/V0\.10\.1/);
+  assert.match(html,/V0\.11\.0/);
   assert.match(html,/fahrenheit-display\.js/);
   assert.match(js,/temperatureC/);
   assert.match(js,/\*9\/5/);
